bdf41a92912d987927faa1d860031ed24e2d1f1f
Qimen Xu <qimenxu@foxmail.com>
2022-08-23 16:40:08 -0400

    some changes during consulting session
    * add a warm-up call in the begining
    * change MPI_SUM to MPI_MIN
    * increase length of vector x
    * change the last reference call from Iallreduce to Allreduce
    * increase wall time in do_some_work()

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Compile the code by `make clean; make`. A binary named `hello` will be created. 
 ```bash
 $ mpirun -np <nproc> ./hello
 ```
-Estimated Memory Usage: 166 MB/proc.
+Estimated Memory Usage: 830 MB/proc.
 
 ### Description of the test
 This is a simple MPI test to test the non-blocking MPI operations. We perform a non-blocking collective operation. While the operation is going on in the background, we do some work (in this case, some random work that takes a lot of time, more than it would take for the collective opertion to be completed). Then we do `MPI_Wait` to make sure the non-blocking operation is completed. Ideally, the time spent at `MPI_Wait` should be close to 0 if overlapping the communication and computation is working as expected. We do the non-blocking operation again without doing any work in between as a reference.
--- hello.c
+++ hello.c
@@ -9,7 +9,7 @@ double do_some_work() {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     double rand_val = 0.0, temp = 0.0;
-    for (int i = 0; i < 2e6; i++) {
+    for (int i = 0; i < 20e6; i++) {
     // for (int i = 0; i < 2e1; i++) {
         srand(i+rank+1);
         temp = ((double) rand() / RAND_MAX);
@@ -32,16 +32,24 @@ int main(int argc, char **argv) {
     t_start = MPI_Wtime();
     
     // test allreduce
-    size_t n = 10000000;
+    size_t n = 50000000;
     // n = 100;
     double *x = malloc(n * sizeof(*x));
     assert(x != NULL);
 
     for (int i = 0; i < n; i++) x[i] = (double) rank;
 
-    double sum = 0.0;
     MPI_Request req;
-    MPI_Iallreduce(MPI_IN_PLACE, x, n, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD, &req);
+
+    // warm up
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Iallreduce(MPI_IN_PLACE, x, n, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD, &req);
+    MPI_Wait(&req, MPI_STATUS_IGNORE);
+
+
+
+    // start the non-blocking operation
+    MPI_Iallreduce(MPI_IN_PLACE, x, n, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD, &req);
     
     // do some work here while the communication is going on
     // sleep(2);
@@ -56,15 +64,17 @@ int main(int argc, char **argv) {
     
     // do it again without doing any work in between 
     MPI_Barrier(MPI_COMM_WORLD);
+    // sleep(10);
     t3 = MPI_Wtime();
-    MPI_Iallreduce(MPI_IN_PLACE, x, n, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD, &req);
-    MPI_Wait(&req, MPI_STATUS_IGNORE);
+    // MPI_Iallreduce(MPI_IN_PLACE, x, n, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD, &req);
+    // MPI_Wait(&req, MPI_STATUS_IGNORE);
+    MPI_Allreduce(MPI_IN_PLACE, x, n, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
     t4 = MPI_Wtime();
-    
-    
+
+  
     t_end = MPI_Wtime();
 
-    printf("sum = %.1f in rank %3d, wait time (overlap): %7.3f ms, actual reduce time (no overlap): %6.3f ms, "
+    printf("min = %.1f in rank %3d, wait time (overlap): %7.3f ms, actual reduce time (no overlap): %6.3f ms, "
             "total time: %.3f ms\n",
             x[n/2], rank, (t2-t1)*1e3, (t4-t3)*1e3, (t_end-t_start)*1e3);
     
